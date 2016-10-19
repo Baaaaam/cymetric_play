0cec1002fe08771b74934f53ff4d1067e44c59c7
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-10-19 18:09:35 -0500

    fix timeer start inv method

--- cy_fast_timed_analysis.py
+++ cy_fast_timed_analysis.py
@@ -1,11 +1,18 @@
 import cymetric as cym
 import pandas as pd
 import numpy as np
+from timeit import default_timer as timer
+import copy
 
 
 def get_transaction_TS(db, sender, receiver):
+  start = timer()
   #initiate evaluation
   evaler = cym.Evaluator(db)
+  
+  now = timer()
+  print('evaluator done:', now-start)
+  start = timer()
   
   # get transation & Agent tables
   trans = evaler.eval('Transactions')
@@ -23,7 +30,10 @@ def get_transaction_TS(db, sender, receiver):
     print("unknown Receiver, available Receiver are:")
     for receiver_name in agents_Receiver.Prototype.unique():
       print(receiver_name)
-  
+  now = timer()
+  print('receiver select done:', now-start)
+  start = timer()
+
   # get sender
   agents_Sender = agents.rename(index=str, columns={'AgentId': 'SenderId'})
   selected_sender = agents_Sender.loc[lambda df: df.Prototype == sender,:]
@@ -32,44 +42,56 @@ def get_transaction_TS(db, sender, receiver):
     print("unknown Sender, available Sender are:")
     for sender_name in agents_Sender.Prototype.unique():
       print(sender_name)
+  now = timer()
+  print('sender select done:', now-start)
+  start = timer()
 
   #check if seider and receiver exist
   if( selected_sender.empty or selected_receiver.empty):
-    trans_table = 0
+    toplot = 0
   else: 
   # Both receiver and sender exist:
     # select good corresponding transaction
     selected_trans = trans.loc[trans['ReceiverId'].isin(selected_receiver.ReceiverId)]
     selected_trans = selected_trans.loc[selected_trans['SenderId'].isin(selected_sender.SenderId)]
+    now = timer()
+    print('trans select done:', now-start)
+    start = timer()
     
     # Merge Sender infos
     df = pd.merge(selected_sender[['SimId', 'SenderId', 'Prototype']], selected_trans, on=['SimId', 'SenderId'])
     df = df.rename(index=str, columns={'Prototype': 'SenderProto'})
     df = df.drop('SenderId',1)
-    
+    now = timer()
+    print('sender merged done:', now-start)
+    start = timer()
     # Merge reveiver infos
     df = pd.merge(selected_receiver[['SimId', 'ReceiverId', 'Prototype']], df, on=['SimId', 'ReceiverId'])
     df = df.rename(index=str, columns={'Prototype': 'ReceiverProto'})
     df = df.drop('ReceiverId',1)
+    now = timer()
+    print('receiver merged done:', now-start)
+    start = timer()
 
     # Get resource and select the proper one
     resource = evaler.eval('Resources')
     selected_resources = resource.loc[resource['ResourceId'].isin(selected_trans.ResourceId)]
+    now = timer()
+    print('resource selected done:', now-start)
+    start = timer()
 
     # merge Resource into transaction
     df = pd.merge(selected_resources[['SimId', 'ResourceId','QualId','Quantity','Units'  ]], df, on=['SimId', 'ResourceId'])
     df = df.drop('ResourceId',1)
+    now = timer()
+    print('resource merged done:', now-start)
+    start = timer()
 
     grouped_trans = df[['ReceiverProto', 'SenderProto','Time', 'Quantity']].groupby(['ReceiverProto', 'SenderProto','Time']).sum()
 
-    trans_table = grouped_trans.loc[receiver].loc[sender]
-  return trans_table
+    toplot = grouped_trans.loc[receiver].loc[sender]
+  
+  
+  return toplot
 
-def get_inventory(db, facility):
-  #initiate evaluation
-  evaler = cym.Evaluator(db)
 
-  # Get inventory table
-  inv = evaler.eval('ExplicitInventory')
-
-  selected_inv = inv.loc[lambda df: df.InventoryName == facility,:]
