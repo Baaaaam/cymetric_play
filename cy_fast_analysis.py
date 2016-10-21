00ed09445df95b9c7ca196fa53d0c7e6ca4926b3
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-10-21 12:54:06 -0500

    working trans from only receiver or sender

--- cy_fast_analysis.py
+++ cy_fast_analysis.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 from pyne import nucname
 
-def get_transaction_TS(db, sender, receiver, *xargs):
+def get_transaction_TS(db, sender='None', receiver='None', *xargs):
 
+  if sender == 'None' and receiver == 'None':
+    print( " Please choose a Receiver or a Sender or both!!")
+    return 0
+  
   nuc_list = []
   for inx, nuc in enumerate(xargs):
     nuc_list.append(nucname.id(nuc))
@@ -17,40 +21,47 @@ def get_transaction_TS(db, sender, receiver, *xargs):
   agents = evaler.eval('AgentEntry')
   
 # build 2 table for SenderId and ReceiverId
-  # get receiver
-  agents_Receiver = agents.rename(index=str, columns={'AgentId': 'ReceiverId'})
-  selected_receiver = agents_Receiver.loc[lambda df: df.Prototype == receiver,:]
-  # check if receiver exists
-  if selected_receiver.empty:
-    print("unknown Receiver, available Receiver are:")
-    for receiver_name in agents_Receiver.Prototype.unique(): #check if the loop is correct should it not be trans.XX.XX
-      print(receiver_name)
-  
+  # get receiveri
+  agents_receiver = agents.rename(index=str, columns={'AgentId': 'ReceiverId'})
+  if receiver != 'None':
+    agents_receiver_ = agents_receiver.loc[lambda df: df.Prototype == receiver,:]
+    # check if receiver exists
+    if agents_receiver_.empty:
+      print("unknown Receiver, available Receiver are:")
+      for receiver_name in agents_receiver.Prototype.unique(): #check if the loop is correct should it not be trans.XX.XX
+        print(receiver_name)
+    else:
+      agents_receiver = agents_receiver_
+
+
   # get sender
-  agents_Sender = agents.rename(index=str, columns={'AgentId': 'SenderId'})
-  selected_sender = agents_Sender.loc[lambda df: df.Prototype == sender,:]
-  # check if sender exists
-  if selected_sender.empty:
-    print("unknown Sender, available Sender are:")
-    for sender_name in agents_Sender.Prototype.unique():
-      print(sender_name)
+  agents_sender = agents.rename(index=str, columns={'AgentId': 'SenderId'})
+  if sender != 'None':
+    agents_sender_ = agents_sender.loc[lambda df: df.Prototype == sender,:]
+    # check if sender exists
+    if agents_sender_.empty:
+      print("unknown Sender, available Sender are:")
+      for sender_name in agents_sender.Prototype.unique():
+        print(sender_name)
+    else:
+      agents_sender = agents_sender_
 
   #check if seider and receiver exist
-  if( selected_sender.empty or selected_receiver.empty):
-    trans_table = 0
+  if( agents_sender.empty or agents_receiver.empty):
+    return 0
   else: 
   # Both receiver and sender exist:
     # select good corresponding transaction
-    selected_trans = trans.loc[trans['ReceiverId'].isin(selected_receiver.ReceiverId)]
-    selected_trans = selected_trans.loc[selected_trans['SenderId'].isin(selected_sender.SenderId)]
+    selected_trans = trans.loc[trans['ReceiverId'].isin(agents_receiver.ReceiverId)]
+    selected_trans = selected_trans.loc[selected_trans['SenderId'].isin(agents_sender.SenderId)]
     
     # Merge Sender infos
-    df = pd.merge(selected_sender[['SimId', 'SenderId', 'Prototype']], selected_trans, on=['SimId', 'SenderId'])
+    df = pd.merge(agents_sender[['SimId', 'SenderId', 'Prototype']], selected_trans, on=['SimId', 'SenderId'])
     df = df.rename(index=str, columns={'Prototype': 'SenderProto'})
     df = df.drop('SenderId',1)
     
     # Merge reveiver infos
-    df = pd.merge(selected_receiver[['SimId', 'ReceiverId', 'Prototype']], df, on=['SimId', 'ReceiverId'])
+    df = pd.merge(agents_receiver[['SimId', 'ReceiverId', 'Prototype']], df, on=['SimId', 'ReceiverId'])
     df = df.rename(index=str, columns={'Prototype': 'ReceiverProto'})
     df = df.drop('ReceiverId',1)
 
@@ -70,7 +81,15 @@ def get_transaction_TS(db, sender, receiver, *xargs):
       df['Quantity'] = df['Quantity'] * df['MassFrac']
     
     grouped_trans = df[['ReceiverProto', 'SenderProto','Time', 'Quantity']].groupby(['ReceiverProto', 'SenderProto','Time']).sum()
-    trans_table = grouped_trans.loc[receiver].loc[sender]
+
+    if sender == 'None':
+      grouped_trans = df[['ReceiverProto','Time', 'Quantity']].groupby(['ReceiverProto','Time']).sum()
+      trans_table = grouped_trans.loc[receiver]
+    elif receiver == 'None':
+      grouped_trans = df[['SenderProto','Time', 'Quantity']].groupby(['SenderProto','Time']).sum()
+      trans_table = grouped_trans.loc[sender]
+    else:
+      trans_table = grouped_trans.loc[receiver].loc[sender]
   
   return trans_table
 
